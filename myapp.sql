3f1e96d98bd8422b079b97e8b88a3c55527ea53b
Devesh-Attri <142967021+Devesh-Attri@users.noreply.github.com>
2023-12-10 02:17:41 +0530

    Add files via upload

---
+++ myapp.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE myappdb;
+USE myappdb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+select * from users;
